5649ebec3d38ed69996fb8dcc30505f411173a47
minGom0302 <qazwsx636@naver.com>
2022-12-26 15:10:39 +0900

    git commit -m "Create Project"

---
+++ app/src/main/java/com/example/black_jack/Activity_Main.java
@@ -0,0 +1,37 @@
+package com.example.black_jack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Activity_Main extends AppCompatActivity {
+    Button startBtn, ruleBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.main_gameStartBtn);
+        ruleBtn = findViewById(R.id.main_gameRuleBtn);
+
+        startBtn.setOnClickListener(v -> {
+            changeLayout(0);
+        });
+        ruleBtn.setOnClickListener(v -> {
+            changeLayout(1);
+        });
+    }
+
+    private void changeLayout(int cnd) {
+        Intent intent = null;
+        if(cnd == 0) {
+            intent = new Intent(Activity_Main.this, Activity_Game.class);
+        } else if(cnd == 1) {
+            intent = new Intent(Activity_Main.this, Activity_Rules.class);
+        }
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/black_jack/Activity_Rules.java
@@ -0,0 +1,14 @@
+package com.example.black_jack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Rules extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+    }
+}
